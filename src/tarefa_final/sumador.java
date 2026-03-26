14bb3e367717d43a3a318e3ff3913f1cfe4ca8a0
toxugpt <gonzasoft2023@gmail.com>
2026-03-26 17:39:07 +0100

    tarefa_commit_primer

---
+++ src/tarefa_final/sumador.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarefa_final;
+
+/**
+ *
+ * @author alvabagon
+ */
+public class sumador {
+
+    public static float sumar(float a, float b) {
+
+        return a + b;
+
+    }
+}
